1ba37503d53626f2579778cbf881a7d2c745c253
AparnaTiwari899 <khushit285@gmail.com>
2024-01-09 18:41:52 +0530

    file added

--- odd-even.js
+++ odd-even.js
@@ -1,7 +1,29 @@
-let n=5;
-if(n%2==0){
-    console.log("even")
+let n=13;
+// let i
+// if(n==1 || n<=0){
+//     console.log("not a prime")
+// }
+// if(i==2){
+//     console.log("it is a prime")
+// }
+// else 
+// for( i=2 ; i<n ;i++){
+//     if(n%i==0){
+//         console.log("not a prime")
+//         break;
+//     }
+  
+// }
+//     console.log("isPrime")
+// if(i==n){
+//     console.log("isPrime")
+// }
+if(num<=1){
+    console.log('i not prime')
 }
-else{
-    console.log("odd")
+for(let i=2;i<num;i++){
+    if(num%i===0){
+         return console.log('is not a prime')
+    }
+    return console.log('is prime')
 }
