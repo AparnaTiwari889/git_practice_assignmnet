679aba13089e7c8f039c26d4687a198d1a66d9e4
erjyotigoswami9 <erjyotigoswami9@gmail.com>
2024-01-09 18:13:25 +0530

    prime program

--- odd-even.js
+++ odd-even.js
@@ -1,7 +1,20 @@
 let n=5;
-if(n%2==0){
-    console.log("even")
-}
-else{
-    console.log("odd")
-}
+if(n==1 || n==0 || n<0) 
+  console.log("Not a Prime")
+else if(n==2)
+       console.log("Prime")
+    else {
+        let flag=true 
+        for(let i=2;i<n;i++){
+            if(n%i==0){
+                console.log("Not a Prime")
+                flag=false 
+                break 
+            }
+        }
+        if(flag==true) 
+          console.log("Prime")
+        else {
+            console.log("Not a Prime")
+        }
+    }
